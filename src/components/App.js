b40e1cceb64c2229fb8966c356fc927dd7c90e04
Nicholas <crazedglitch@yahoo.com>
2023-09-05 06:11:56 -0500

    init home component & css

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Import components
+import Home from './Home';
+// Import CSS
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Home() {
+
+    return (
+    <body>
+        <header class="header">
+            <div class="logo-box">
+                <img src="img/logo-white.png" alt="Logo" class="logo" />
+            </div>
+            <div class="text-box">
+                <h1 class="heading-primary">
+                    <span class="heading-primary-main">Outdoors</span>
+                    <span class="heading-primary-sub">is where life happens</span>
+                </h1>
+
+                <a href="#" class="btn btn-white btn-animated">Discover our tours</a>
+            </div>
+        </header>
+    </body>
+    )
+}
+
+export default Home;
