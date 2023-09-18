981f8a7b1cc4d728a95073c9c6ca259e4a9274d5
Nicholas <crazedglitch@yahoo.com>
2023-09-18 08:12:01 -0500

    SASS nesting complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,12 +13,14 @@ function Home() {
             </div>
             <div className="header__text-box">
                 <h1 className="heading-primary">
+                <br></br>
                     <span className="heading-primary--main">Nicholas Martin</span>
+                    <br></br>
                     <span className="heading-primary--sub">Software Engineer</span>
-                    <span className="heading-secondary--sub">&</span>
-                    <span className="heading-secondary--sub">Website Developer</span>
+                    <span className="heading-primary--sub">&</span>
+                    <span className="heading-primary--sub">Website Developer</span>
                 </h1>
-                <a href="#" className="btn btn--white btn--animated">Download my resume</a>
+                <a href="#" className="btn btn--white btn--animated">Download Resume</a>
                 <br></br>
                 <br></br>
                 <a href="#" className="btn btn--white btn--animated">Explore my creations</a>
