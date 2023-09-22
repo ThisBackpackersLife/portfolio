02429d8f797d6e550299c38f6d54cd9b5599f216
Nicholas <crazedglitch@yahoo.com>
2023-09-22 06:51:19 -0500

    Header & Column's added

--- src/sass/components/About.js
+++ src/sass/components/About.js
@@ -5,9 +5,19 @@ import '../main.scss';
 function About() {
     return (
         <section className="section-about">
-            <h2 className="heading-secondary">
-                Meet the Engineer
-            </h2>
+            <div className="u-center-text u-margin-bottom-8">
+                <h2 className="heading-secondary">
+                    Meet the Engineer
+                </h2>
+            </div>
+            <div class="row">
+                    <div class="col-1-of-2">
+                        Text Content
+                    </div>
+                    <div class="col-1-of-2">
+                        Image composition
+                    </div>
+                </div>
         </section>
     )
 }
