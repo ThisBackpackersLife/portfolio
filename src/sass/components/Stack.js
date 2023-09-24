8f11e6002219725b508ead81ccf40e2819d09bd7
Nicholas <crazedglitch@yahoo.com>
2023-09-24 07:04:42 -0500

    _feature-box.scss created

--- src/sass/components/Stack.js
+++ src/sass/components/Stack.js
@@ -5,9 +5,20 @@ import '../main.scss';
 function Stack() {
 
     return (
-        <section className="section-features">
-            <i className="icon-basic-world"></i>
-        </section>
+        <section class="section-features">
+                
+                <div class="row">
+                    <div class="col-1-of-4">
+                        <div class="feature-box">
+                            <i class="feature-box__icon icon-basic-world"></i>
+                            <h3 class="heading-tertiary">Flask Python</h3>
+                            <p class="feature-box__text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
     )
 }
 
