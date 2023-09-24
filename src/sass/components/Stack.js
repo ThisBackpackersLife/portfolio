a773158d30953b8e97a8d35c40150f8859497979
Nicholas <crazedglitch@yahoo.com>
2023-09-24 07:43:16 -0500

    4 feature-boxes created w/ margin

--- src/sass/components/Stack.js
+++ src/sass/components/Stack.js
@@ -17,6 +17,36 @@ function Stack() {
                             </p>
                         </div>
                     </div>
+
+                    <div className="col-1-of-4">
+                        <div className="feature-box">
+                            <i className="feature-box__icon icon-basic-world"></i>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Flask Python</h3>
+                            <p className="feature-box__text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="col-1-of-4">
+                        <div className="feature-box">
+                            <i className="feature-box__icon icon-basic-world"></i>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Flask Python</h3>
+                            <p className="feature-box__text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="col-1-of-4">
+                        <div className="feature-box">
+                            <i className="feature-box__icon icon-basic-world"></i>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Flask Python</h3>
+                            <p className="feature-box__text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </section>
     )
