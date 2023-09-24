ca5fd54d918342bc550617d95c94323661a28a48
Nicholas <crazedglitch@yahoo.com>
2023-09-24 07:34:08 -0500

    feauture-box styling added

--- src/sass/components/Stack.js
+++ src/sass/components/Stack.js
@@ -5,14 +5,14 @@ import '../main.scss';
 function Stack() {
 
     return (
-        <section class="section-features">
-                
-                <div class="row">
-                    <div class="col-1-of-4">
-                        <div class="feature-box">
-                            <i class="feature-box__icon icon-basic-world"></i>
-                            <h3 class="heading-tertiary">Flask Python</h3>
-                            <p class="feature-box__text">
+        <section className="section-features">
+                <h2 className="feature-heading u-center-text">My Stack</h2>
+                <div className="row">
+                    <div className="col-1-of-4">
+                        <div className="feature-box">
+                            <i className="feature-box__icon icon-basic-world"></i>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Flask Python</h3>
+                            <p className="feature-box__text">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             </p>
                         </div>
