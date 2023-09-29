f77274160def91bd8fed831f1f39b7c6f7ced7fe
Nicholas <crazedglitch@yahoo.com>
2023-09-29 10:21:36 -0500

    added html to projects component

--- src/sass/components/Projects.js
+++ src/sass/components/Projects.js
@@ -3,12 +3,27 @@ import React from "react";
 // import '../main.scss';
 
 function Projects() {
-    console.log('Projects component rendered')
 
     return (
-        <section>
-            
-        </section>
+        <section class="section-tours">
+                <div class="u-center-text u-margin-bottom-big">
+                    <h2 class="heading-secondary">
+                        My software creations
+                    </h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                    <div class="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                    <div class="col-1-of-3">
+                        Col 1 of 3
+                    </div>
+                </div>
+            </section>
     )
 }
 
