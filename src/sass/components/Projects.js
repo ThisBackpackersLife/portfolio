37c371144c5bce805519f8648ec9e27a7320d29f
Nicholas <crazedglitch@yahoo.com>
2023-10-10 06:13:13 -0500

    card html added

--- src/sass/components/Projects.js
+++ src/sass/components/Projects.js
@@ -16,7 +16,15 @@ function Projects() {
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card__side card__side--front">
-                                Front
+                                <div className="card__picture">
+
+                                </div>
+                                <div className="card__heading">
+
+                                </div>
+                                <div className="card__details">
+                                    
+                                </div>
                             </div>
                             <div className="card__side card__side--back card__side--back-1">
                                 Back
