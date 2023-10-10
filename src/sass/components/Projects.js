093d0b29fd4f63a4a333f4bd0412287c5b893529
Nicholas <crazedglitch@yahoo.com>
2023-10-10 06:41:20 -0500

    card styling updated

--- src/sass/components/Projects.js
+++ src/sass/components/Projects.js
@@ -16,14 +16,14 @@ function Projects() {
                     <div className="col-1-of-3">
                         <div className="card">
                             <div className="card__side card__side--front">
-                                <div className="card__picture">
-
+                                <div className="card__picture card__picture--1">
+                                    &nbsp;
                                 </div>
                                 <div className="card__heading">
-
+                                    Heading
                                 </div>
                                 <div className="card__details">
-                                    
+                                    Details
                                 </div>
                             </div>
                             <div className="card__side card__side--back card__side--back-1">
