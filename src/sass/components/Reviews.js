22a19ef6bd526b0ad9357ea9413c29a3788c1047
Nicholas <crazedglitch@yahoo.com>
2023-10-18 08:16:45 -0500

    review placeholder added

--- src/sass/components/Reviews.js
+++ src/sass/components/Reviews.js
@@ -9,7 +9,15 @@ function Reviews() {
             </div>
             <div class="row">
                 <div class="review">
-                    TEXT
+                    <figure className="review__shape">
+
+                    </figure>
+                    <div className="review_text">
+                        <h3 className="heading-tertiary">Sterling Barton: Full-Stack Software Engineer</h3>
+                        <p className="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate exercitationem, asperiores dolorum nisi fuga facilis sint voluptates eos porro, reiciendis libero incidunt corrupti voluptas aliquam! Voluptates similique ut error!
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
