6d30b3fe3dbe9c541c773e8c7281a2af1eb9d109
Nicholas <crazedglitch@yahoo.com>
2023-10-18 07:47:57 -0500

    init section-reviews styling

--- src/sass/components/Reviews.js
+++ src/sass/components/Reviews.js
@@ -3,7 +3,7 @@ import React from "react";
 function Reviews() {
 
     return(
-        <section className="section-stories">
+        <section className="section-reviews">
             <h2 className="feature-heading u-center-text u-margin-top-big">Reviews</h2>
         </section>
     )
