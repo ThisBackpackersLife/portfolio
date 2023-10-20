e1efa742f12250ad1b0da0da58096c25d46ac22c
Nicholas <crazedglitch@yahoo.com>
2023-10-20 07:48:20 -0500

    Name input added to form

--- src/sass/components/Contact.js
+++ src/sass/components/Contact.js
@@ -8,7 +8,10 @@ function Contact() {
                 <div className="contact">
                         <div className="contact__form">
                             <form action="#" className="form">
-                                
+                                <div className="form__group">
+                                <input type="text" className="form__input" placeholder="Full Name" id="name" required />
+                                <label for="name" className="form__label">Full Name</label>
+                                </div>
                             </form>
                         </div>
                 </div>
