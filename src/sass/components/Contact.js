937ff4d51e1959da0e61f137a726b9a782b2e02e
Nicholas <crazedglitch@yahoo.com>
2023-10-20 07:56:27 -0500

    form heading added

--- src/sass/components/Contact.js
+++ src/sass/components/Contact.js
@@ -8,6 +8,12 @@ function Contact() {
                 <div className="contact">
                         <div className="contact__form">
                             <form action="#" className="form">
+                                <div className="u-center-text u-margin-bottom-big">
+                                    <h2 className="heading-secondary">
+                                        Contact Nicholas
+                                    </h2>
+                                </div>
+
                                 <div className="form__group">
                                     <input type="text" className="form__input" placeholder="Full Name" id="name" required />
                                     <label for="name" className="form__label">Full Name</label>
