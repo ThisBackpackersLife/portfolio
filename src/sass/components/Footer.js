25b3ec8ed8b5aee594033ed213bcd27947337385
Nicholas <crazedglitch@yahoo.com>
2023-10-23 09:34:34 -0500

    projects added to footer

--- src/sass/components/Footer.js
+++ src/sass/components/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
                         <li className="footer__item"><a href="#" className="footer__link">Home</a></li>
                         <li className="footer__item"><a href="#" className="footer__link">About</a></li>
                         <li className="footer__item"><a href="#" className="footer__link">My Stack</a></li>
-                        
+                        <li className="footer__item"><a href="#" className="footer__link">Projects</a></li>
                     </ul>
                     </div>
                 </div>
