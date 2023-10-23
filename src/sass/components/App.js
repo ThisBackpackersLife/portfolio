28ea72c49da4cf1f0bb13d0a04a22c4af9d3345a
Nicholas <crazedglitch@yahoo.com>
2023-10-23 12:25:08 -0500

    prebuilt grid removed from App.js

--- src/sass/components/App.js
+++ src/sass/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // Import components
 import Home from './Home';
-// import Grid from './Grid';
 import About from './About';
 import Stack from './Stack';
 import Projects from './Projects';
@@ -13,7 +12,6 @@ function App() {
   return (
     <div>
       <Home />
-      {/* <Grid /> */}
       <About />
       <Stack />
       <Projects />
