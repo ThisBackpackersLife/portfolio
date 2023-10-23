c82f7b9f64ce7bda72c99b6c7f6bc4fee785ab64
Nicholas <crazedglitch@yahoo.com>
2023-10-23 09:06:18 -0500

    footer logo added

--- src/sass/components/Footer.js
+++ src/sass/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
+// Import images here:
+import logo from "../../images/NMSE.png";
 
 function Footer() {
 
     return (
         <footer className="footer">
-            <p>Hello</p>
+            <div className="footer__logo-box">
+                <img src={ logo } alt="Nicholas Logo" class="footer__logo" />
+            </div>
         </footer>
     )
 }
