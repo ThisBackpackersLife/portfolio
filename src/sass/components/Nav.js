1bc548c4b4817f0e98193c310cc2fb3b8dc701b4
Nicholas <crazedglitch@yahoo.com>
2023-10-25 07:11:01 -0500

    Nav label && ul added

--- src/sass/components/Nav.js
+++ src/sass/components/Nav.js
@@ -5,6 +5,12 @@ function Nav() {
     return (
         <div className="navigation">
             <input type="checkbox" className="navigation__checkbox" id="nav-toggle" />
+
+            <label for="nav-toggle" className="navigation__btn">Menu</label>
+
+            <div className="navigation__nav">
+                <ul className="navigation__list"></ul>
+            </div>
         </div>
     )
 }
