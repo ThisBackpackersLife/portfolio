fad2a9696cfd9f331ba05ece381176b11638688e
Nicholas <crazedglitch@yahoo.com>
2023-10-27 13:11:36 -0500

    p element replaced with div in Stack.js

--- src/sass/components/Stack.js
+++ src/sass/components/Stack.js
@@ -12,14 +12,14 @@ function Stack() {
                         <div className="feature-box">
                             <i className="feature-box__icon icon-basic-archive-full"></i>
                             <h3 className="heading-tertiary-2 u-margin-bottom-small">Flask Python</h3>
-                            <p className="feature-box__text">
+                            <div className="feature-box__text">
                                 <ul className="feature-box__list">
                                     <li className="feature-box__list-item">CRUD</li>
                                     <li className="feature-box__list-item">Authentication</li>
                                     <li className="feature-box__list-item">Custom/Private Routes</li>
                                     <li className="feature-box__list-item">RESTful API's & Web Services</li>
                                 </ul>
-                            </p>
+                            </div>
                         </div>
                     </div>
 
@@ -27,14 +27,14 @@ function Stack() {
                         <div className="feature-box">
                             <i className="feature-box__icon icon-basic-gear"></i>
                             <h3 className="heading-tertiary-2 u-margin-bottom-small">React</h3>
-                            <p className="feature-box__text">
+                            <div className="feature-box__text">
                                 <ul className="feature-box__list">
                                     <li className="feature-box__list-item">Redux</li>
                                     <li className="feature-box__list-item">Context</li>
                                     <li className="feature-box__list-item">Custom Hooks</li>
                                     <li className="feature-box__list-item">Component Architecture</li>
                                 </ul>
-                            </p>
+                            </div>
                         </div>
                     </div>
 
@@ -42,14 +42,14 @@ function Stack() {
                         <div className="feature-box">
                             <i className="feature-box__icon icon-basic-pencil-ruler"></i>
                             <h3 className="heading-tertiary-2 u-margin-bottom-small">CSS/SASS</h3>
-                            <p className="feature-box__text">
+                            <div className="feature-box__text">
                                 <ul className="feature-box__list">
                                     <li className="feature-box__list-item">BEM</li>
                                     <li className="feature-box__list-item">7-1 Structure</li>
                                     <li className="feature-box__list-item">Mixins & Nesting</li>
                                     <li className="feature-box__list-item">Reusable Components</li>
                                 </ul>
-                            </p>
+                            </div>
                         </div>
                     </div>
 
@@ -57,14 +57,14 @@ function Stack() {
                         <div className="feature-box">
                             <i className="feature-box__icon icon-basic-floppydisk"></i>
                             <h3 className="heading-tertiary-2 u-margin-bottom-small">SQL</h3>
-                            <p className="feature-box__text">
-                            <ul className="feature-box__list">
+                            <div className="feature-box__text">
+                                <ul className="feature-box__list">
                                     <li className="feature-box__list-item">Querying</li>
                                     <li className="feature-box__list-item">Database Management</li>
                                     <li className="feature-box__list-item">Performance Optimization</li>
                                     <li className="feature-box__list-item">Data Modeling & Architecture</li>
                                 </ul>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
