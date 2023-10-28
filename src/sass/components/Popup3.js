fc0df0d63ea6b87293ea0c2abcd16a5b8a8359e7
Nicholas <crazedglitch@yahoo.com>
2023-10-28 09:16:24 -0500

    popup3 img-container added

--- src/sass/components/Popup3.js
+++ src/sass/components/Popup3.js
@@ -6,7 +6,9 @@ function Popup3() {
         <div className="popup" id="popup3">
             <div className="popup__content">
                 <div className="popup__left">
-                    
+                    <div className="popup__img-container">
+                        
+                    </div>
                 </div>
             </div>
         </div>
