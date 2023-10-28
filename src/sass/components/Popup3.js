c6de120e1c1122aec621b51f964aa22e8006de0f
Nicholas <crazedglitch@yahoo.com>
2023-10-28 09:15:08 -0500

    popup3 content div added

--- src/sass/components/Popup3.js
+++ src/sass/components/Popup3.js
@@ -4,7 +4,9 @@ import React from "react";
 function Popup3() {
     return (
         <div className="popup" id="popup3">
+            <div className="popup__content">
 
+            </div>
         </div>
     )
 }
