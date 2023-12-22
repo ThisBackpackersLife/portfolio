4ca829e8227dcd727ed268aa9cebe7066ec51b15
Nicholas <crazedglitch@yahoo.com>
2023-12-22 12:31:08 -0600

    reviews p spacing typos fixed

--- src/sass/components/Reviews.js
+++ src/sass/components/Reviews.js
@@ -31,7 +31,7 @@ function Reviews() {
                     <div className="review__text">
                         <h3 className="heading-tertiary-2 u-margin-bottom-small">Sterling Barton: Software Engineer 👩‍💻📱🌐</h3>
                         <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate exercitationem, asperiores dolorum nisi fuga facilis sint voluptates eos porro, reiciendis libero incidunt corrupti voluptas aliquam! Voluptates similique ut error! Quas cupiditate exercitationem, asperiores dolorum nisi fuga facilis sint voluptates eos porro, reiciendis libero incidunt corrupti voluptas aliquam! Voluptates similique ut error.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate exercitationem, asperiores dolorum nisi fuga facilis sint voluptates eos porro, reiciendis libero incidunt corrupti voluptas aliquam! Voluptates similique ut error! Quas cupiditate exercitationem, asperiores dolorum nisi fuga facilis sint voluptates eos porro, reiciendis libero incidunt corrupti voluptas aliquam! Voluptates similique ut error.
                         </p>
                     </div>
                 </div>
