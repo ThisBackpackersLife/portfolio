46bc23228ad4c4d957db58403cc65aa5d896019f
Nicholas <crazedglitch@yahoo.com>
2024-01-07 12:05:21 -0600

    contact id added to popup1 contact btn

--- src/sass/components/Popup1.js
+++ src/sass/components/Popup1.js
@@ -30,7 +30,7 @@ function Popup1() {
                     </p>
                     <div className="popup__btn-container">
                         <a href="#section-reviews" className="btn btn--green popup__btn-1">Reviews</a>
-                        <a href="#" className="btn btn--green popup__btn-2">Contact</a>
+                        <a href="#section-contact" className="btn btn--green popup__btn-2">Contact</a>
                     </div>
                 </div>
             </div>
