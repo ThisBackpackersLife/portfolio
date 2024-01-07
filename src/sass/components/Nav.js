ee763085adcf08bcd986ccf933cee83f342fa171
Nicholas <crazedglitch@yahoo.com>
2024-01-07 13:02:22 -0600

    Connected nav projects link

--- src/sass/components/Nav.js
+++ src/sass/components/Nav.js
@@ -17,7 +17,7 @@ function Nav() {
                     <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
                     <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
                     <li className="nav__item"><a href="#stack" className="nav__link">My Stack</a></li>
-                    <li className="nav__item"><a href="#" className="nav__link">Apps</a></li>
+                    <li className="nav__item"><a href="#projects" className="nav__link">Projects</a></li>
                     <li className="nav__item"><a href="#" className="nav__link">Reviews</a></li>
                     <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
                 </ul>
