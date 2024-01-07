db34487d7cf8b742d1fb82db48b269d6bc1bf23e
Nicholas <crazedglitch@yahoo.com>
2024-01-07 12:01:36 -0600

    reviews id added to popup1 review btn

--- src/sass/components/Popup1.js
+++ src/sass/components/Popup1.js
@@ -29,7 +29,7 @@ function Popup1() {
                         Learn-Link is an award winning inclusive and AI-driven educational platform meticulously crafted to impart elementary mathematical education for individuals with diverse accessibility needs. Embodying a profound commitment to breaking down barriers, our platform is driven by a resolute mission to empower skill enhancement, foster self-assurance, and pave pathways toward a brighter future for our audience. At the core of Learn-Link's distinction lies her multifaceted role, transcending the conventions of an AI-powered chatbot. Named Adda, our chatbot assumes the persona of a friendly and guiding companion, tailor-made for each user's learning journey. Her adaptability to individual learning styles, adeptness in simplifying intricate concepts into approachable explanations, her unwavering support and patience, redefine the learning experience.
                     </p>
                     <div className="popup__btn-container">
-                        <a href="#" className="btn btn--green popup__btn-1">Reviews</a>
+                        <a href="#section-reviews" className="btn btn--green popup__btn-1">Reviews</a>
                         <a href="#" className="btn btn--green popup__btn-2">Contact</a>
                     </div>
                 </div>
