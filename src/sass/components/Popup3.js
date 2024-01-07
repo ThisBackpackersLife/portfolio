6a3209408c1c11b9e2cf6752b9c3ea6e496959de
Nicholas <crazedglitch@yahoo.com>
2024-01-07 12:57:14 -0600

    popup3 contact link fixed

--- src/sass/components/Popup3.js
+++ src/sass/components/Popup3.js
@@ -30,7 +30,7 @@ function Popup3() {
                     </p>
                     <div className="popup__btn-container">
                         <a href="#reviews" className="btn btn--blue popup__btn-1">Reviews</a>
-                        <a href="#section-contact" className="btn btn--blue popup__btn-2">Contact</a>
+                        <a href="#contact" className="btn btn--blue popup__btn-2">Contact</a>
                     </div>
                 </div>
             </div>
