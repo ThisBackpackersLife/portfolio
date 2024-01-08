8c2e3abbfb2609ca208222575d5a82cf77a3c462
Nicholas <crazedglitch@yahoo.com>
2024-01-08 15:10:52 -0600

    nav links hidden when closed

--- src/sass/components/Nav.js
+++ src/sass/components/Nav.js
@@ -23,16 +23,18 @@ function Nav() {
 
             <div className="nav__bg" onClick={ closeMenu } >&nbsp;</div>
 
-            <div className={ `nav__nav ${ isMenuOpen ? 'open' : '' } ` }>
-                <ul className="nav__list">
-                    <li className="nav__item"><a href="#" className="nav__link" onClick={ closeMenu }>Home</a></li>
-                    <li className="nav__item"><a href="#about" className="nav__link" onClick={ closeMenu }>About</a></li>
-                    <li className="nav__item"><a href="#stack" className="nav__link" onClick={ closeMenu }>My Stack</a></li>
-                    <li className="nav__item"><a href="#projects" className="nav__link" onClick={ closeMenu }>Projects</a></li>
-                    <li className="nav__item"><a href="#reviews" className="nav__link" onClick={ closeMenu }>Reviews</a></li>
-                    <li className="nav__item"><a href="#contact" className="nav__link" onClick={ closeMenu }>Contact</a></li>
-                </ul>
-            </div>
+            { isMenuOpen && (
+                <div className={ `nav__nav ${ isMenuOpen ? 'open' : '' } ` }>
+                    <ul className="nav__list">
+                        <li className="nav__item"><a href="#" className="nav__link" onClick={ closeMenu }>Home</a></li>
+                        <li className="nav__item"><a href="#about" className="nav__link" onClick={ closeMenu }>About</a></li>
+                        <li className="nav__item"><a href="#stack" className="nav__link" onClick={ closeMenu }>My Stack</a></li>
+                        <li className="nav__item"><a href="#projects" className="nav__link" onClick={ closeMenu }>Projects</a></li>
+                        <li className="nav__item"><a href="#reviews" className="nav__link" onClick={ closeMenu }>Reviews</a></li>
+                        <li className="nav__item"><a href="#contact" className="nav__link" onClick={ closeMenu }>Contact</a></li>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
